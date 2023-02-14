460e2012d9aaf09ed24b387329fec395977989a8
Thomaz Alves <thomazhilario5@gmail.com>
2023-02-14 14:07:17 -0300

    tipos de dados em javascript

---
+++ Exercícios/Tipos de dados.js
@@ -0,0 +1,101 @@
+/*
+    Data types // tipos de dados
+        *Primitive
+        *structural
+        *structural primitive
+
+    *Dados primitivos
+        *String
+        *number
+        *boolean
+        *underfined
+        *Symbol
+        *BigInt
+        
+    *Dados Estruturais
+        #Object
+            *Array
+            *map
+            *set
+            *Date
+            *...
+        #Function
+        
+    *Primitive structural
+        *Null    
+*/
+
+/*
+    String 
+
+    *cadeia de caracteres
+
+    //escrever textos em java script deve usar:
+
+    ""//aspas duplas
+    ''//aspas simples
+    ´´/template literals
+*/
+console.log("ola!")
+console.log('me chamo Thomaz')
+console.log(`e estou começando a estudar JavaScript`)
+
+/* 
+    Number:Numeros
+
+    pode ser:
+    inteiros // 33
+    reais // 12.3
+    NaN // Not a Number - não é um número
+*/
+console.log(33) // inteiro
+console.log(12.5) // reais
+console.log(33 / "asf") // NaN
+
+/*
+    Boolean
+
+        *Somente 2 valores
+
+            true // verdadeiro
+            false // falso
+
+*/
+
+console.log(true)
+console.log(false)
+
+/*
+    Underfined
+        *indefinido
+        algo que não existe
+*/
+
+/*
+    null
+      *Nulo
+      *algo que existe mas não tem nada dentro
+*/
+
+/*
+    Object
+        *objeto
+        *Propriedades / atributos
+        *Funcionalidades / métodos
+
+        {propriedade: "valor"}
+*/
+console.log({
+    nome: "thomaz",
+    idade: "18 anos",
+    Escolaridade:"ensino medio completo",
+})
+
+/*
+    Array (vetores)
+        * uma lista
+        *agrupamento de dados
+
+        ["leite","ovos","açucar",2,3]
+*/
+console.log(["2 leite","12 ovos","1 açucar","1 massa de bolo","1 confetes",1,2,3,4])
