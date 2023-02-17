3c4f86b753ba1dca08a397b64c832c44a2815a08
Thomaz Alves <thomazhilario5@gmail.com>
2023-02-17 10:56:01 -0300

    functions

---
+++ Exercícios/script.js
@@ -0,0 +1,31 @@
+/* Fuctions (Funções) 
+    #é um agrupamento de código
+    #posso reutilizar este código
+    #dar significado/ um nome pra minha função
+*/
+    function createnames() {
+        console.log("thomaz")
+        console.log("Albevânia")
+        console.log("Valdevino")
+        console.log("Jõao")
+    }
+
+/* function: expression
+    #é uma função dentro de uma variável
+
+*/
+
+   let min = function() {
+
+   }
+
+/* function:parameters (parâmetros)
+    #Parâmetros são dados em funções.
+*/    
+
+    function sum (number1,number2){
+        console.log(number1 + number2)
+    }
+    //number1 e number2 são parâmetros/dados da função
+
+    sum(2,1) //-> argument ou argumentos para os meus parâmetros.
