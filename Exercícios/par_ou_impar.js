691af4788fda565bfb258db0d6d4489080f2e302
Thomaz Alves <thomazhilario5@gmail.com>
2023-03-12 17:46:15 -0300

    update code

--- Exercícios/par_ou_impar.js
+++ Exercícios/par_ou_impar.js
@@ -1,3 +1,4 @@
+/* Olá meu nome é Thomaz,e este exercício peço ao usuário digitar um número e o meu código devolve como resposta se é par ou impar */
 function parOrImpar(){
     let number = window.prompt("Digite um número:")
     if(number % 2 == 0){
