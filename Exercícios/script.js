f87af10296fb2ec46a05aae25435b430e1df164c
Thomaz Alves <thomazhilario5@gmail.com>
2023-03-16 14:14:17 -0300

    estrutura de repentição:while

--- Exercícios/script.js
+++ Exercícios/script.js
@@ -4,12 +4,23 @@
     #Para declarar o for precisamos nos atentar em três detalhes ao declara-lo;for(inicializar um variável,condição para sua continuação,expressão final).
     #dentro do for podemos colocar condicionais e outros valores como break para parar a execução e o continue que irá pular a execução de momento.
 */
-let x = 10;
-for(x; x > 0;x--){
+for(let x = 10; x > 0;x--){
     if(x == 7){
         continue
     }else if (x == 4){
         break
     }
     console.log(x)
+}
+
+
+/* While 
+    # Significa enquanto
+    # usa quando não sabe o momento da parada.
+    # Declaramos a variável antes de declarar o while
+*/
+let n = 10
+while(n > 5){
+    console.log(n)
+    n--
 }
