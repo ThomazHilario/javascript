98c0e4c049014dc9f7300b6e45aff1a20bc3b9a1
Thomaz Alves <thomazhilario5@gmail.com>
2023-03-22 22:37:49 -0300

    pratica:Maximo entre dois números

---
+++ Exercícios/Praticas/maximo-entre-dois-valores.js
@@ -0,0 +1,10 @@
+//Escreva uma função que usa dois números e retorne o maior entre eles
+
+function maioroumenor(number1,number2){
+    if(number1 > number2){
+        console.log(number1)
+    }else{
+        console.log(number2)
+    }
+}
+maioroumenor(34,15)
