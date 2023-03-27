38365cdbf2f5f05f630add275591781d25de69d5
Thomaz Alves <thomazhilario5@gmail.com>
2023-03-27 14:41:41 -0300

    Entrada convertida em tempo

---
+++ Exercícios/Praticas/Entrada-Convertida-em-Tempo.js
@@ -0,0 +1,25 @@
+function verifyenter(number){
+    if(typeof number === "number"){
+        return number
+    } else{
+        throw "A entrada não é um número"
+    }
+}
+
+function conversionEnterInTime(number){
+    let enter = verifyenter(number)
+
+    let horas = Math.floor(enter / 3600)
+    enter = enter % 3600
+
+    let minutos = Math.floor(enter / 60)
+    enter = enter % 60
+
+    console.log(`${horas}:Horas ${minutos}:Minutos ${enter}:Segundos`)
+}
+
+try{
+    conversionEnterInTime(3600)
+}catch(e){
+    console.log(e)
+}
