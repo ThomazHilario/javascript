d62848eb6f67fbe99b6a44f57afc5a0bc6975a9c
Thomaz Alves <thomazhilario5@gmail.com>
2023-03-28 13:31:41 -0300

    Número primo

---
+++ Exercícios/Praticas/números-primos.js
@@ -0,0 +1,19 @@
+//Crie uma função que retorne se um número é primo ou não
+function numberPrimo(number){
+    for(let i = 2;i <= number;i++){
+        let ehprimo = true
+        for(let divisor = 2;divisor < number;divisor++){
+            if(number % divisor === 0)
+            ehprimo = false
+        }
+        if(ehprimo === true){
+            console.log(`${number} é Primo`)
+            break
+        } else{
+            console.log(`${number} não é primo`)
+            break
+        }
+    }
+}
+
+numberPrimo()
