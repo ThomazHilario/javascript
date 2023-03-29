93bcc766bb4f1d2b7f3361107fba28e6601c9d3e
Thomaz Alves <thomazhilario5@gmail.com>
2023-03-29 07:44:26 -0300

    trabalhando com arrays e repetições

--- Exercícios/script.js
+++ Exercícios/script.js
@@ -83,3 +83,105 @@ try{
 }
 
 
+/* 
+    ∞ Buscando e contando dados em Arrays ∞
+    Baseado no Array de Livros por Categoria abaixo, faça os seguintes desafios
+        • Contar o número de categorias e o número de livros em cada categoria
+        • Contar o número de autores
+        • Mostrar livros do autor Auguto Cury
+        • Transformar a função acima em uma função que irá receber o nome do autor e devolver os livros desse autor.
+*/
+
+const booksByCategory = [
+    {
+        category: "Riqueza",
+        books: [
+            {
+                title: "Os segredos da mente milionária",
+                author: "T. Harv Eker",
+            },
+            {
+                title: "O homem mais rico da Babilônia",
+                author: "George S. Clason",
+            },
+            {
+                title: "Pai rico, pai pobre",
+                author: "Robert T. Kiyosaki e Sharon L. Lechter",
+            },
+        ],
+    },
+    {
+        category: "Inteligência Emocional",
+        books: [
+            {
+                title: "Você é Insubstituível",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Ansiedade – Como enfrentar o mal do século",
+                author: "Augusto Cury",
+            },
+            {
+                title: "Os 7 hábitos das pessoas altamente eficazes",
+                author: "Stephen R. Covey",
+            },
+        ],
+    },
+];
+
+//contando as categorias e seus livros
+
+let totalCategory = booksByCategory.length
+console.log(totalCategory)
+for(let i of booksByCategory){
+    console.log("Categoria: " + i.category)
+    console.log("Total de livros: " + i.books.length)
+}
+
+//Contando autores
+
+function contAuthors(){
+    let authors = []
+    for(let category of booksByCategory){
+        for(let book of category.books){
+            if(authors.indexOf() == -1){
+                authors.push(book.author)
+            }
+        }
+    }
+    console.log("total de autores: " + authors.length)
+}
+
+contAuthors()
+
+//Contando livros de augustu cury
+
+function augustuCury(){
+    let books = []
+    for(let category of booksByCategory){
+        for(let book of category.books){
+            if(book.author === "Augusto Cury"){
+                books.push(book.title)
+            }
+        }
+    }
+    console.log(books)
+}
+
+augustuCury()
+
+//Transformar a função acima em uma função que irá receber o nome do autor e devolver os livros desse autor.
+
+function seachbook(name){
+    let books = []
+    for(let category of booksByCategory){
+        for(let book of category.books){
+            if(book.author === name){
+                books.push(book.title)
+            }
+        }
+    }
+    console.log(books)
+}
+
+seachbook("T. Harv Eker")
