40303b87ca8c4ae020374a474f1950c82c29c61c
Thomaz Alves <thomazhilario5@gmail.com>
2023-03-30 15:20:59 -0300

    organizando repositório

---
+++ Exercícios/DOM/script.js
@@ -0,0 +1,6 @@
+/* Introdução ao DOM */
+// DOM - Document Object Model.
+// é um HTML convertido em objeto javascript.
+// A representação do DOM é tipo uma árvore de pais e filhos, criada pelo browser.
+// Com o DOM podemos manipular o html usando o javascript.
+
