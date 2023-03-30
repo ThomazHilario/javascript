d521e34b2793acfe34d258921d31ee04b2f7ed9c
Thomaz Alves <thomazhilario5@gmail.com>
2023-03-30 15:40:47 -0300

    Introdução ao DOM

--- Exercícios/DOM/Introdução ao dom/script.js
+++ Exercícios/DOM/Introdução ao dom/script.js
@@ -4,3 +4,6 @@
 // A representação do DOM é tipo uma árvore de pais e filhos, criada pelo browser.
 // Com o DOM podemos manipular o html usando o javascript.
 
+//Buscar por ID-document.getElementById()
+
+console.log(document.getElementById('title'))
