701a1ac35316503c25a2b8e22416533a5ad606b2
Thomaz Alves <thomazhilario5@gmail.com>
2023-04-05 16:08:23 -0300

    classlist

--- Exercícios/DOM/Manipulandos estilos e classes/script.js
+++ Exercícios/DOM/Manipulandos estilos e classes/script.js
@@ -5,3 +5,8 @@ let body = document.getElementsByTagName('body')[0]
 
 body.style.background = 'lightgreen'
 
+//alterando estilos com o classlist
+
+let texto = document.getElementsByTagName('h1')[0]
+
+texto.classList.add('texto')
