634dd997ae92c3e2610ccee0091e8ecc83acbee9
Thomaz Alves <thomazhilario5@gmail.com>
2023-04-07 16:47:34 -0300

    navegando pelos elementos

---
+++ Exercícios/DOM/Navegando pelos elementos/script.js
@@ -0,0 +1,15 @@
+/* Navegando pelos elementos */
+
+//pegar elementos pais usando o parentNode ou parentElement
+
+let titulo = document.querySelector('h1')
+
+console.log(titulo.parentElement)
+
+//Pegando elementos filhos com childNodes.
+
+let body = document.getElementsByTagName('body')[0]
+
+console.log(body.childNodes)
+console.log(body.children)
+console.log(body.firstElementChild)
