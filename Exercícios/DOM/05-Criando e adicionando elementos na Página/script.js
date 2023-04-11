c42679b65c59b6bf67c1d73d9b336651cd6165b1
Thomaz Alves <thomazhilario5@gmail.com>
2023-04-11 18:44:43 -0300

    insertBefore()

--- Exercícios/DOM/05-Criando e adicionando elementos na Página/script.js
+++ Exercícios/DOM/05-Criando e adicionando elementos na Página/script.js
@@ -11,3 +11,11 @@ div.innerText = 'elemento adicionado'
 let body = document.getElementsByTagName('body')[0]
 
 body.append(div)
+
+//Caso eu queira adicionar um elemento criado entre uma tag e outra basta usar insertBefore(),e ele recebe dois parâmetros o elementos que será adicionado e a tag na qual será adicionada antes desta.
+
+let h3 = document.createElement('h3')
+
+body.insertBefore(h3,div)
+
+h3.innerHTML = 'Elemento adicionado antes do h2'
