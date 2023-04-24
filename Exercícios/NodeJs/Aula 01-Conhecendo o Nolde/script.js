b78553cdd7c64e9c8d34a2f15fce6467e1a0e55f
Thomaz Alves <thomazhilario5@gmail.com>
2023-04-24 17:09:53 -0300

    Introdução a nodeJS

---
+++ Exercícios/NodeJs/Aula 01-Conhecendo o Nolde/script.js
@@ -0,0 +1,10 @@
+/* Para que serve o NodeJS */
+
+// Desenvolver back-end e front-end com o javascript.
+// Desenvolver API.(API é um local que disponibiliza dados para que outro possam consumir esses dados)
+// Não é recomendado usar node caso tenha muito processamento.
+
+/* Vantagens do NodeJS */
+// Rápido na execução.
+// Alta escalabilidade.
+// Aplicações de ponta. 
