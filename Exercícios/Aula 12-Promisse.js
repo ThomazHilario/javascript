2eebfa383373043cc5692eb3d6403d97376bfe03
Thomaz Alves <thomazhilario5@gmail.com>
2023-05-02 16:40:44 -0300

    Promisse

---
+++ Exercícios/Aula 12-Promisse.js
@@ -0,0 +1,10 @@
+/* Promisse */
+// É um objeto JavaScript com a promessa de que algo vai acontecer.
+// Usado para Operações assíncronas.(carregar arquivo e leitura de dados de uma API)
+
+/* Estados de uma promisse */
+
+// Pending: Estado inicial, assim que o objeto da promessa é iniciado/criado.
+// Fulfilled: A promessa foi concluida com sucesso
+// Reject: A promessa foi rejeitada.
+// Settled: seja com sucesso ou com erro,ela foi finalmente concluida.
