cafa39c5c4f1bfc0a6e1bdcf47c2f84668dc2ecb
Thomaz Alves <thomazhilario5@gmail.com>
2023-05-11 14:36:58 -0300

    Introdução á API

---
+++ Exercícios/Aula 13-Introdução a API's.js
@@ -0,0 +1,14 @@
+/* Introdução a API's */
+
+/* O que é uma Api ? E para que server ? */
+
+// Api permite trocar informações entre sistemas, ajuda a fazer transações de dados de sistemas nos quais não temos acesso.
+
+// O programador faz uma requisição(pedido) a uma API, esta API vai mandar um pedido ao servidor o, servidor irá preparar os dados e devolver para a API que vai entregar o pedido do programador.
+
+/* Arquivo JSON */
+
+// Arquivo que armazena informações e dados
+// Declarado por chaves {}
+// Formado por chaves e valores {"chaves": "valor",}
+// Pode-se ter objetos e arrays
