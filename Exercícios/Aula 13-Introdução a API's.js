3e676b819bbf329965f9ed3c9e914379163131ce
Thomaz Alves <thomazhilario5@gmail.com>
2023-05-12 16:39:03 -0300

    Métodos HTTP

--- Exercícios/Aula 13-Introdução a API's.js
+++ Exercícios/Aula 13-Introdução a API's.js
@@ -6,10 +6,18 @@
 
 // O programador faz uma requisição(pedido) a uma API, esta API vai mandar um pedido ao servidor o, servidor irá preparar os dados e devolver para a API que vai entregar o pedido do programador.
 
-/* Arquivo JSON */
+/* Arquivo.JSON */
 
 // JavaScript Object Notation.
 // Arquivo que armazena informações e dados.
 // Declarado por chaves {}.
 // Formado por chaves e valores {"chaves": "valor",}.
 // Pode-se ter dados do tipo objetos,arrays,numeros,string,null.
+
+/* Métodos HTTP */
+
+// Get: a API fornece informações.
+// POST: A API recebe informações que devem ser registradas ou não.
+// DELETE: A API recebe identificadores de registros que devem ser apagados.
+// PUT: A API recebe informações para update de um ou mais registros.
+// PATCH: A API recebe informações de update de um registro.
