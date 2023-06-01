a7aeac2e37db136cc7b26967bf00c3a2d5cefd73
Thomaz Alves <thomazhilario5@gmail.com>
2023-06-01 18:31:02 -0300

    Filter

--- Exercícios/Aula 15-Funções map, filter e reduce.js
+++ Exercícios/Aula 15-Funções map, filter e reduce.js
@@ -10,6 +10,22 @@ console.log(lista_numeros.map(r => Number(r)))
 
 // Importante dizer que o map é uma função que recebe um argumento e este argumento pode ser tratado: map(r => r)
 
+/* Filter */
+//Serve para filtrar um array com base em true e false para cada elemento do array, A função pode passar até três parâmetros,filter((item,index,array)),vale lembrar que o filter retornará um novo array, por isto deve armazenar este novo array em uma variável.
+
+// No exercício abaixo iremos filtrar os nomes ds pessoas que tiveram nota acima de 6
+
+const resultadoProvas = [
+    {name:'Thomaz',nota:9.5},
+    {name:'Lincoln',nota:10},
+    {name:'Luiz',nota:5.5},
+    {name:'Arthur',nota:5},
+]
+
+const alunosAprovados = resultadoProvas.filter(aluno => aluno.nota > 6)
+
+console.log(alunosAprovados)
+
 /* reduce */
 // A função reduce permite percorrer um array, retornando um unico valor que pode ser o total de uma soma,divisão,multiplicação e etc, entre os elementos.
 //A função reduce recebe dois argumento sendo o primeiro o valor que ira receber a soma do segundo argumento
