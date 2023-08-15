c8301144e7bdaa47155c46e6364e5b6bad5fb5e4
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-15 11:47:46 -0300

    Aprendendo a inicializar um pacote com npm

---
+++ Exercícios/Ferramenta para front-end/NPM/02 - Iniciando um pacote com npm/index.js
@@ -0,0 +1,7 @@
+/*
+    -> Iniciando um pacote com npm
+    - Podemos inicializar um pocote npm de duas maneiras:
+    -> no terminal:
+    - 1 - maneira: npm init
+    - 2 - maneira: npm init -y
+*/
