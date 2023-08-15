7321b5e62a98b91743039951f80d626331f2266c
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-15 11:54:26 -0300

    Entendendo o package.json

---
+++ Exercícios/Ferramenta para front-end/NPM/03 - Entendendo o package.json/index.js
@@ -0,0 +1,7 @@
+/*
+    -> Entendendo o package.json
+    - Ao inicializar o pacote com npm será cirado um arquivo package.json
+    - json significa: javascript Objetc notation, arquivo de notação em javascript com chave e valor,semelhante a um objeto.
+    - Seu uso é guarda/armazenar informações ou representar informações.
+    - Neste arquivo podemos modificar algumas informações relativas ao package.json, como name, main,author, license e etc.
+*/
