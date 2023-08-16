dce2d2ee33b3cdd8331774ef12fd0caaf6cd393d
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-16 10:36:44 -0300

    Dependencias de desenvolvimento

--- Exercícios/Ferramenta para front-end/NPM/04 - Instalando dependências/index.js
+++ Exercícios/Ferramenta para front-end/NPM/04 - Instalando dependências/index.js
@@ -2,4 +2,8 @@
     -> instalando dependências
     - No terminal:
     - npm install (nome do modulo)
+    - npm i (nome do modulo)
+
+    -> Dependencias de desenvolvimento
+    - npm i (nome do modulo) -D : este código significa que só irá usar esta dependência em ambiente de desenvolvimento
 */
