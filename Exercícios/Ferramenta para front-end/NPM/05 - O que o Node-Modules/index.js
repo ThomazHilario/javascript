40a69ad7920a5db1dd7e6f0e341274bb2afa244f
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-16 10:53:28 -0300

    Entendendo a pasta node_modules

---
+++ Exercícios/Ferramenta para front-end/NPM/05 - O que o Node-Modules/index.js
@@ -0,0 +1,7 @@
+/*
+    -> Node Modules
+    - Pasta que é criada ao instalar um dependência/modulo/package.
+    - Esta pasta irá armazenar os modulo instalados.
+    - Ao instalar um certo modulo, este poderá instalar outros e armazenar na pasta node_modules.
+    - A pasta node_modules não é enviado ao repositório.
+*/
