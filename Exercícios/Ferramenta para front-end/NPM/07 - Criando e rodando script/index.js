14ecb10a4b5dc4f7624c2ee9b56100d0a6113fd9
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-17 11:25:17 -0300

    Criando e rodando scripts

---
+++ Exercícios/Ferramenta para front-end/NPM/07 - Criando e rodando script/index.js
@@ -0,0 +1,5 @@
+/*
+    -> Criando e rodando scripts na package.json
+    - Podemos criar um script e roda-lo na aba de script na package.json.
+    - No terminal usar npm run (valor da chave do script)
+*/
