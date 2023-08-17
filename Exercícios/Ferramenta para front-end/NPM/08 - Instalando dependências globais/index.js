95ce528c8ee6aa44838dde9284c00dd6b1b036b3
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-17 13:50:42 -0300

    Instalando e desinstalando dependências globais

---
+++ Exercícios/Ferramenta para front-end/NPM/08 - Instalando dependências globais/index.js
@@ -0,0 +1,9 @@
+/*
+    -> Instalando dependências globais
+    - no terminal:
+    - npm i (nome do modulo) -g
+
+    -> desinstalar a dependência global
+    - no terminal:
+    - npm uninstall (nome do modulo) -g
+*/
