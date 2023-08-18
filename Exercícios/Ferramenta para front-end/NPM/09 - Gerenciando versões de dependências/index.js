3687c54cac482556778a54c9d74850093b3ac257
Thomaz Alves <thomazhilario5@gmail.com>
2023-08-18 12:42:21 -0300

    gerenciando versões das dependências

---
+++ Exercícios/Ferramenta para front-end/NPM/09 - Gerenciando versões de dependências/index.js
@@ -0,0 +1,6 @@
+/**
+    -> Gerenciando versões das dependências
+    - No terminal :
+    - npm outdated: verifica quais versões disponiveis e que podem ser usadas. 
+    - npm i (nome do modulo)@ latest: instala a ultima versão da dependência
+ */
